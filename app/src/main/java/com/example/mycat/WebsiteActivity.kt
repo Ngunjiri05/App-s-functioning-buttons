7e6f5f0cda4e6f22bdcce4522b1cb71e44e5d98b
Ngunjiri05 <sylviawanjikumn@gmail.com>
2023-04-14 19:04:29 +0300

    first commit

---
+++ app/src/main/java/com/example/mycat/WebsiteActivity.kt
@@ -0,0 +1,20 @@
+package com.example.mycat
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+
+
+class WebsiteActivity : AppCompatActivity() {
+    lateinit var myWeb:WebView
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_website)
+        myWeb = findViewById(R.id.mWeb)
+        val webSettings = myWeb.settings
+        webSettings.javaScriptEnabled = true
+        myWeb.loadUrl("https://www.emobilis.ac.ke")
+    }
+}
